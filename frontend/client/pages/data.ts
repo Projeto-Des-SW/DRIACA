6dbd681e865ec0faefa75c92264c3b47a2c9a737
Geisianny Bernardo <74117146+Geisianny@users.noreply.github.com>
2025-09-14 19:22:25 -0300

    atualização de perguntas do FAQ

--- frontend/client/pages/data.ts
+++ frontend/client/pages/data.ts
@@ -1,32 +1,32 @@
 export const faqData = [
   {
-    question: "Como faço para me matricular na universidade?",
+    question: "Como enviar atividades complementares?",
     answer:
-      "Para se matricular, primeiro você precisa ser aprovado no vestibular ou ENEM. Após aprovação, acesse o portal do aluno com seu CPF e senha fornecida. Complete o processo enviando os documentos obrigatórios.",
+      "Solicitante abre processo, enviando e-mail para processo@ufape.edu.br, com:\n- 1- Formulário preenchido (disponível em www.ufape.edu.br/drca); e\n- 2- Cópia dos Certificados de ACC e de ACEX\nEntão, o discente deve enviar um e-mail para processo@ufape.edu.br com o formulário preenchido e cópia dos certificados de ACC e ACEX.",
   },
   {
-    question: "Quais são os prazos do calendário acadêmico 2024?",
+    question: "Como solicitar comprovante de matrícula ao DRCA?",
     answer:
-      "1º Semestre: Matrículas de 15 a 30 de janeiro, início das aulas em 05 de fevereiro, término em 15 de julho. 2º Semestre: Matrículas de 15 a 30 de julho, início das aulas em 05 de agosto, término em 15 de dezembro.",
+      "Para solicitar comprovante de matrícula ao DRCA, é necessário seguir as etapas descritas na plataforma SOLICITA: Solicitar a matrícula e escolher a opção de solicitar comprovante de matrícula.",
   },
   {
-    question: "Como funciona o sistema de bolsas de estudo?",
+    question: "Como desbloquear o siga?",
     answer:
-      "Oferecemos diversas modalidades: ProUni, FIES, bolsas de mérito, assistência estudantil e iniciação científica.",
+      "De acordo com o contexto fornecido, para desbloquear o SIGA, o discente deve acessar o módulo escolaridade do SOLICITA e realizar o pedido de desbloqueio. O DRCA irá realizar o desbloqueio do SIGA e enviará um e-mail informando os passos a seguir para completar o procedimento. O discente deve seguir as instruções enviadas pelo e-mail e criar uma nova senha.",
   },
    {
-    question: "Como faço para me matricular na universidade?",
+    question: "Esqueci a senha do siga, e agora?",
     answer:
-      "Para se matricular, primeiro você precisa ser aprovado no vestibular ou ENEM. Após aprovação, acesse o portal do aluno com seu CPF e senha fornecida. Complete o processo enviando os documentos obrigatórios.",
+      "De acordo com o contexto fornecido, para desbloquear a senha do SIGA, você deve acessar o módulo escolaridade do SOLICITA (https://solicita.ufape.edu.br) e realizar o pedido de desbloqueio. O Departamento de Registro e Controle Acadêmico (DRCA) irá realizar o desbloqueio do SIGA e enviará um e-mail com as instruções para completar o procedimento, incluindo a criação de uma nova senha.",
   },
    {
-    question: "Como faço para me matricular na universidade?",
+    question: "Vocês avisam quando o diploma está pronto pra retirar?",
     answer:
-      "Para se matricular, primeiro você precisa ser aprovado no vestibular ou ENEM. Após aprovação, acesse o portal do aluno com seu CPF e senha fornecida. Complete o processo enviando os documentos obrigatórios.",
+      "Sim, o DRCA envia um email informando sobre o arquivamento do processo de solicitação do diploma, o que significa que o diploma está pronto para retirada.",
   },
    {
-    question: "Como faço para me matricular na universidade?",
+    question: "Quais documentos são necessários para retirar o diploma?",
     answer:
-      "Para se matricular, primeiro você precisa ser aprovado no vestibular ou ENEM. Após aprovação, acesse o portal do aluno com seu CPF e senha fornecida. Complete o processo enviando os documentos obrigatórios.",
+      "Para os egressos que concluíram o curso a partir de 2019.1, os seguintes documentos são necessários para retirar o diploma: - Documento oficial com foto; - Comprovante de Assinatura da Ata de Colação de Grau; - Comprovante de Devolução da Beca; - Nada consta da Biblioteca (pode ser solicitado através de www.solicita.ufape.edu.br); - Comprovante de Depósito do TCC na Biblioteca (pode ser solicitado através de www.solicita.ufape.edu.br).",
   },
 ];
